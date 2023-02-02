2591faa45ad33f25a34e3319c1e00d741291e80d
Sinan Aldulaimi <aldulasi@sheridancollege.ca>
2023-02-02 10:07:14 -0500

    Second commit

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,18 @@
+
+
+package javaapplication4;
+
+/**
+ * This class *** Insert class description here ***
+ * @author Sinan Aldulaimi
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
